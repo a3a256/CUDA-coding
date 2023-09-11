14368535bee7e8d8bc9e76daf81701a03f09281b
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-09-12 01:29:04 +0600

    changed dims and stuff about to test it in the future, need to add threads and blocks set up in main

--- cuda_demonstration/cuda_statistics/statistical_functions.cu
+++ cuda_demonstration/cuda_statistics/statistical_functions.cu
@@ -3,20 +3,33 @@
 #include "device_launch_parameters.h"
 #include <cstdio>
 
-cudaError_t cudaMean1d(int* a, int *b, const int size);
+cudaError_t cudaMean1d(int* a, int *b, const int m, const int n, dim3 threads, dim3 blocks);
 
-__global__ void sum1dKernel(int *a, int *val) {
-	int i = threadIdx.x;
-	*val += a[i];
-	printf("%d %d\n", a[i], *val);
+__global__ void sum1dKernel(int *a, int *val, const int m, const int n) {
+	int row = blockDim.y * blockIdx.y + threadIdx.y;
+	int col = blockDim.x * blockIdx.x + threadIdx.x;
+	if (col < n && row < m) {
+		val[col] = a[n * row + col];
+	}
 }
 
 int main() {
-	const int m = 5;
-	int arr[m] = { 1, 2, 3, 4, 5 };
-	int res = -1;
+	const int m = 4;
+	const int n = 3;
+	int arr[m][n] = { {1, 2, 3}, {4, 2, 6}, {7, 2, 9}, {9, 5, 5} };
+	int res[n] = { 0 };
 
-	cudaError_t status = cudaMean1d(arr, &res, m);
+	int a[m * n];
+	int i, j, k;
+	k = 0;
+	for (i = 0; i < m; i++) {
+		for (j = 0; j < n; j++) {
+			a[k] = arr[i][j];
+			k++;
+		}
+	}
+
+	cudaError_t status = cudaMean1d(a, res, m, n);
 	printf("\n");
 
 	if (status != cudaSuccess) {
@@ -35,7 +48,7 @@ int main() {
 	return 0;
 }
 
-cudaError_t cudaMean1d(int* a, int* b, const int size) {
+cudaError_t cudaMean1d(int* a, int* b, const int m, const int n, dim3 threads, dim3 blocks) {
 	int *dev_a = 0;
 	int *dev_c = 0;
 
@@ -47,27 +60,27 @@ cudaError_t cudaMean1d(int* a, int* b, const int size) {
 		goto Error;
 	}
 
-	status = cudaMalloc((void**)&dev_a, size * sizeof(int));
+	status = cudaMalloc((void**)&dev_a, m * n * sizeof(int));
 
 	if (status != cudaSuccess) {
 		fprintf(stderr, "Failed to allocate memory\n");
 		goto Error;
 	}
 
-	status = cudaMalloc((void**)&dev_c, sizeof(int));
+	status = cudaMalloc((void**)&dev_c, n * sizeof(int));
 
 	if (status != cudaSuccess) {
 		fprintf(stderr, "Failed to allocate memory\n");
 		goto Error;
 	}
 
-	status = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+	status = cudaMemcpy(dev_a, a, m * n * sizeof(int), cudaMemcpyHostToDevice);
 	if (status != cudaSuccess) {
 		fprintf(stderr, "Failed to copy from host to GPU buffer\n");
 		goto Error;
 	}
 
-	sum1dKernel << <1, size >> > (dev_a, dev_c);
+	sum1dKernel << <blocks, threads >> > (dev_a, dev_c);
 
 	status = cudaGetLastError();
 	if (status != cudaSuccess) {
